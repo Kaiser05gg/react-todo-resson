e9e2789fabfc5bcaa10a7b01807083a99bffd6a5
Kaiser05gg <shu.iss1121@icloud.com>
2026-04-28 22:59:02 +0900

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:4000/api/";
+
+type Todo = {
+  id?: number;
+  titile: string;
+  description: string;
+};
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const click = async () => {
+    const response = await axios.get<Todo[]>("/todos");
+    const todos = response.data;
+    setTodos(todos);
+    console.log(todos);
+  };
   return (
     <>
       <h2 className="red">test</h2>
+      <button onClick={click}>button</button>
+      {todos.map((todo: Todo) => (
+        <li key={todo.id}>
+          {todo.titile},{todo.description}
+        </li>
+      ))}
     </>
   );
 }
